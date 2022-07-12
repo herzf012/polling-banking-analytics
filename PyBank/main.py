cfa9b55e0bd2e9458272cd03a096d335033abd46
Lief Herzfeld <liefherzfeld@gmail.com>
2022-07-12 11:02:31 -0500

    Ignore this add

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,5 @@
+# ----------Set-up----------
+
 # Import relavent modules
 
 import os
@@ -11,7 +13,6 @@ budget_dict = {}
 month_list = []
 profit_losses_list = []
 
-
 with open(budget_csv, encoding = "utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     next(csvreader)
@@ -19,7 +20,6 @@ with open(budget_csv, encoding = "utf-8") as csvfile:
         month_list.append(row[0])
         profit_losses_list.append(int(row[1]))
         budget_dict[row[0]] = row[1]
-
 
 # ----------Analysis----------
 
@@ -46,14 +46,12 @@ while count < len(profit_losses_list):
     delta_profits_losses_list.append(delta)
     count += 1
 
-
 for i in delta_profits_losses_list:
     sum_of_deltas += i
 
 average_delta = sum_of_deltas / len(delta_profits_losses_list)
 
 # Greatest increase and decrease in profits
-# Work in-progress
 
 max_delta = max(delta_profits_losses_list)
 max_delta_index = delta_profits_losses_list.index(max_delta)
@@ -63,8 +61,26 @@ min_delta = min(delta_profits_losses_list)
 min_delta_index = delta_profits_losses_list.index(min_delta)
 min_delta_date = month_list[min_delta_index + 1]
 
-print(f"Greatest Increase in Profits: {max_delta_date} (${max_delta})")
-print(f"Greatest Decrease in Profits: {min_delta_date} (${min_delta})")
+# ----------Output----------
 
+# Put results in a list
 
+results_list = ["Financial Analysis", "----------------------------",
+f"Total months: {total_months}", f"Total: ${total_net_amount}",
+f"Average Change: ${round(average_delta, 2)}", 
+f"Greatest Increase in Profits: {max_delta_date} (${max_delta})",
+f"Greatest Decrease in Profits: {min_delta_date} (${min_delta})"]
 
+# Print results to terminal
+
+for i in results_list:
+    print(i)
+
+# Export results as a text file
+
+results_file = os.path.join("Resources", "results.txt")
+
+with open(results_file, "w") as txtfile:
+    for i in results_list:
+        txtfile.write(i)
+        txtfile.write("\n")
