c0d37221383b3a34f0114fbd6df51d831fdac5da
Lief Herzfeld <liefherzfeld@gmail.com>
2022-07-12 09:30:32 -0500

    Pulled data for workability.

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,12 +3,25 @@
 import os
 import csv
 
-# Set csv path and get it ready to be worked with
+# Set csv path and pull data to work with.
 
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
+budget_dict = {}
+month_list = []
+profit_losses_list = []
+
+
 with open(budget_csv, encoding = "utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
+    next(csvreader)
     for row in csvreader:
-        print(row[0])
+        month_list.append(row[0])
+        profit_losses_list.append(int(row[1]))
+        budget_dict[row[0]] = row[1]
+
+print(profit_losses_list)
+
+# Analysis
+# Do analysis
 
