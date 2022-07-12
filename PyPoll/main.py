b170c7dc464d3ca4118fefea2b30a6c59e56895b
Lief Herzfeld <liefherzfeld@gmail.com>
2022-07-12 13:25:24 -0500

    Stores header row when reading csv file.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,6 +9,8 @@ import csv
 
 election_csv = os.path.join("Resources", "election_data.csv")
 
+header_bool = True
+election_header = []
 ballot_id_dict = {}
 ballot_id_list = []
 county_list = []
@@ -16,12 +18,17 @@ candidate_list = []
 
 with open(election_csv, encoding = "utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
-    next(csvreader)
     for row in csvreader:
-        ballot_id_list.append(row[0])
-        county_list.append(row[1])
-        candidate_list.append(row[2])
-        ballot_id_dict[row[0]] = [row[1], row[2]]
+        if header_bool == True:
+            election_header.append(row[0])
+            election_header.append(row[1])
+            election_header.append(row[2])
+            header_bool = False
+        else:
+            ballot_id_list.append(row[0])
+            county_list.append(row[1])
+            candidate_list.append(row[2])
+            ballot_id_dict[row[0]] = [row[1], row[2]]
 
 # ----------Analysis----------
 
